20d48eaafca53965163613d79aa4ebdfbddae8b8
spangled <1298826065@qq.com>
2022-03-20 12:35:12 +0800

    second commit

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="recommend">
+    <div
+      v-for="(item, index) in recommends"
+      :key="index"
+      class="recommend-item"
+    >
+      <!-- <div v-for="item in recommends" class="recommend-item"> -->
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+        <div>{{ item.title }}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style>
+.recommend {
+  display: flex;
+  text-align: center;
+  font-size: 15px;
+  padding: 10px 0 15px;
+  border-bottom: 10px solid #eee;
+}
+
+.recommend-item {
+  flex: 1;
+}
+
+.recommend-item img {
+  width: 80%;
+  margin-bottom: 5px;
+}
+</style>
